d9b42b91cb718296a3206ead9f1f416ef2c86b31
Carl <carlmas02@gmail.com>
2023-03-23 18:51:01 +0530

    code starter

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def signup(request):
+    return render(request,'signup.html')
+
+def loginpage(request):
+    return render(request,'login.html')
+
+def add(request):
+    return render(request,'add.html')
+
+def delete(request,id):
+    pass
+
+def logout_user(request):
+    return render(request,'login.html')
